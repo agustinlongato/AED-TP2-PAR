92b70ed54c239c340535af6842036560ca7a4c43
agustinlongato <aguslongato@hotmail.com>
2015-04-23 00:11:31 -0300

    TP2-PAR

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "introduzca un numero" << endl ;
+	int N;
+	cin >> N ;
+	if ( N % 2== 0)
+	cout << N << " es par.";
+	else
+	cout << N << " es impar.";
+}
